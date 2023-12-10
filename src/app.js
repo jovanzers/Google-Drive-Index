819bd85a2133d36ce001fe80515f91f3584d4c93
@Jovanzers <jovanzers@gmail.com>
2023-12-10 23:29:11 +0700

    Update title bar pattern

--- src/app.js
+++ src/app.js
@@ -257,9 +257,9 @@ function title(path) {
 	// $('title').html(document.siteName + ' - ' + path);
 	var model = window.MODEL;
 	if (model.is_search_page)
-		$('title').html(`${drive_name} - Search results for ${model.q} `);
+		$('title').html(`Search: ${model.q} - ${UI.siteName}`);
 	else
-		$('title').html(`${drive_name} - ${path}`);
+		$('title').html(`${drive_name}: ${path} - ${UI.siteName}`);
 }
 
 // Render the navigation bar
